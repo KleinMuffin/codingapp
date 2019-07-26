f026f7146d8e2b3395ad96427082cf37ae52c26f
Patrice Voss <voss@ironshark.de>
2019-07-26 14:13:57 +0200

    removed uneeded package

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
-const initialUrl = 'https://thecodinglove.com/page/1?s=';
-const settings = require('electron-settings');
 const pageUrl = 'https://thecodinglove.com/page/';
+const searchQuery = '?s=';
+const initialUrl = pageUrl + '1' + searchQuery;
 
 let nextUrl = initialUrl;
-
-console.log("hello");
-
-function getTime() {
-  return settings.get('time', 30 * 1000)
-}
 
 function reload() {
   console.log("reaload");
@@ -19,9 +13,15 @@ function reload() {
       let random = Math.floor(Math.random() * Math.floor(8));
       const $ = cheerio.load(response.data);
       let randomEl = $('.blog-post:not(.results-return)')[random];
+
+      // need help
       let image = $(randomEl).children('.blog-post-content').children().children().children('object').attr('data');
       if (!image) {
         image = $(randomEl).children('.blog-post-content').children().children().children('img').attr('src');
+
+        if (!image) {
+          image = $(randomEl).children('.blog-post-content').children().children('img').attr('src');
+        }
       }
 
       document.querySelector('#image').src = image;
@@ -29,13 +29,13 @@ function reload() {
 
       random = Math.floor(Math.random() * Math.floor(50)) + 1;
 
-      nextUrl = pageUrl + random + '?s=';
+      nextUrl = pageUrl + random + searchQuery;
     })
     .catch((error) => {
       console.log(error);
       reject(error);
     });
-  setTimeout(reload, getTime())
+  setTimeout(reload, 30000)
 }
 
 reload()
